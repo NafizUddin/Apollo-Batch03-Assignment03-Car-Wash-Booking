f20ea8b0d2bb609f599a1bec485d2629b6025bc0
NafizUddin <nafizuddin.ctg@gmail.com>
2024-06-11 21:39:34 +0600

    added global error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './middlewares/globalErrorHandler';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World Mama!');
 });
+
+app.use(globalErrorHandler);
 
 export default app;
