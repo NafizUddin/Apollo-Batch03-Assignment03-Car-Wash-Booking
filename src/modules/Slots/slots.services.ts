1dee99f06124d4acc9a5db19ff337c2129eac8e0
NafizUddin <nafizuddin.ctg@gmail.com>
2024-08-26 20:15:16 +0600

    fixed get slot service function

--- src/modules/Slots/slots.services.ts
+++ src/modules/Slots/slots.services.ts
@@ -1,38 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { SlotAppointment } from './slots.model';
 
-const getAvailableSlotsFromDB = async (query: Record<string, unknown>) => {
-  if (query?.date && query?.serviceId) {
-    const result = await SlotAppointment.find({
-      date: query?.date,
-      service: query?.serviceId,
-    }).populate('service');
+interface QueryParams {
+  date?: string; // Comma-separated string of dates
+  serviceId?: string; // Service ID as a string
+}
 
-    if (result.length === 0) {
-      return null;
-    }
+const getAvailableSlotsFromDB = async (query: QueryParams) => {
+  const dateArray = query?.date ? query?.date.split(',') : null; // Split the date string into an array
 
-    return result;
-  } else if (query?.date) {
-    const result = await SlotAppointment.find({
-      date: query?.date,
-    }).populate('service');
+  const queryConditions: Record<string, any> = {};
 
-    if (result.length === 0) {
-      return null;
-    }
-    return result;
-  } else if (query?.serviceId) {
-    const result = await SlotAppointment.find({
-      service: query?.serviceId,
-    }).populate('service');
-
-    if (result.length === 0) {
-      return null;
-    }
-    return result;
+  if (dateArray && dateArray.length > 0) {
+    queryConditions.date = { $in: dateArray }; // Use $in operator for matching multiple dates
   }
 
-  const result = await SlotAppointment.find().populate('service');
+  if (query?.serviceId) {
+    queryConditions.service = query?.serviceId;
+  }
+
+  const result =
+    await SlotAppointment.find(queryConditions).populate('service');
 
   if (result.length === 0) {
     return null;
