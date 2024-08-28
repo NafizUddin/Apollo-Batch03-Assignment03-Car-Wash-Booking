8abfb4eb392e1afa501d5294dabf7e51ec8287e6
NafizUddin <nafizuddin.ctg@gmail.com>
2024-08-29 02:54:43 +0600

    fixed some bugs in confirmation page

--- src/modules/individualUserBookings/individualUserBooking.route.ts
+++ src/modules/individualUserBookings/individualUserBooking.route.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { USER_ROLE } from '../Auth/auth.constant';
 import auth from '../../middlewares/auth';
-import { BookingControllers } from '../Booking/booking.controllers';
 import { individualUserBookingsControllers } from './individualUserBooking.controllers';
 
 const router = express.Router();
