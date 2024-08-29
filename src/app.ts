04e33529bdbd02719c25bb6be21228d713f8a4bd
NafizUddin <nafizuddin.ctg@gmail.com>
2024-08-30 00:53:44 +0600

    placed live frontend link in cors origin

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173', // Allow requests from this origin
+    origin: [
+      'https://turboshine-apollob3a5.netlify.app',
+      'http://localhost:5173',
+    ], // Allow requests from this origin
     credentials: true, // Allow cookies and other credentials
   }),
 );
